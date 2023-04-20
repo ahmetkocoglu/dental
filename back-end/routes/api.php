f02246f1639988b6b50afc59be47274eb90f1792
akocoglu <akocoglu@gmail.com>
2023-04-20 22:22:25 +0300

    rout edit

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login')->name('login');
+    Route::post('login', 'login')->name('api-login');
 });
